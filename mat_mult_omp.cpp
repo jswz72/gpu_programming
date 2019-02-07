b21f5bf50434e65ced6bc7322dd15cd7f9666bcc
jswz72 <jacobdasword@gmail.com>
2019-02-07 14:42:47 -0500

    matrix x matrix in omp

---
+++ mat_mult_omp.cpp
@@ -0,0 +1,113 @@
+/**
+  Jacob Sword
+  Parallelized multiplication of two randomized matrices given dimensions of each using OMP
+**/
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <cassert>
+#include <cstdlib>
+#include <time.h>
+#include "omp.h"
+
+using std::cout;
+using std::endl;
+
+void mat_mult(int *mat_a, int *mat_b, int *result, int a_rows, int a_cols, int b_cols)
+{
+    for (int i = 0; i < a_rows; i++) {
+        for (int j = 0; j < b_cols; j++) {
+            int temp_res = 0;
+            for (int k = 0; k < a_cols; k++) {
+                temp_res += mat_a[i * a_cols + k] * mat_b[k * b_cols + j];
+            }
+            result[i * b_cols + j] = temp_res;
+        }
+    }
+}
+
+void mat_mult_omp(int *mat_a, int *mat_b, int *result, int a_rows, int a_cols, int b_cols) {
+#pragma omp parallel
+    {
+        int tid = omp_get_thread_num();
+        while (tid < a_rows) {
+            for (int j = 0; j < b_cols; j++) {
+                int temp_res = 0;
+                for (int k = 0; k < a_cols; k++) {
+                    temp_res  += mat_a[tid * a_cols + k] * mat_b[k * b_cols + j];
+                }
+                result[tid * b_cols + j] = temp_res;
+            }
+            tid += omp_get_num_threads();
+        }
+    }
+}
+
+int main (int argc, char **argv) {
+    if (argc < 5) {
+        cout << "Need a_row a_col b_row b_col" << endl;
+        return 1;
+    }
+    int a_rows = atoi(argv[1]);
+    int a_cols = atoi(argv[2]);
+
+    int b_rows = atoi(argv[3]);
+    int b_cols = atoi(argv[4]);
+
+    if (a_cols != b_rows) {
+        cout << "Columns of matrix a must equal rows of matrix b" << endl;
+        return 1;
+    }
+
+    int a_dims = a_rows * a_cols;
+    int b_dims = b_rows * b_cols;
+    int c_dims = a_rows * b_cols;
+
+    int *a = (int *) malloc(sizeof(int) * a_dims);
+    int *b = (int *) malloc(sizeof(int) * b_dims);
+    int *c = (int *) malloc(sizeof(int) * c_dims);
+
+    srand(time(NULL));
+
+    // Initialize matrix a
+    cout << "Matrix a:" << endl;
+    for (int i = 0; i < a_rows; i++) {
+        for (int j = 0; j < a_cols; j++) {
+            int el = rand() % 10;
+            a[i * a_cols + j] = el;
+            cout << el << ", ";
+        }
+        cout << endl;
+    }
+
+    // Initialize matrix b
+    cout << "Matrix b:" << endl;
+    for (int i = 0; i < b_rows; i++) {
+        for (int j = 0; j < b_cols; j++) {
+            int el = rand() % 5;
+            b[i * b_cols + j] = el;
+            cout << el << ", ";
+        }
+        cout << endl;
+    }
+
+    mat_mult_omp(a, b, c, a_rows, a_cols, b_cols);
+
+
+    int *test_res = (int *) malloc(sizeof(int) * c_dims);
+    // Make sure parallel and sequential implementation same (for testing)
+    mat_mult(a, b, test_res, a_rows, a_cols, b_cols);
+    for (int i = 0; i < a_rows; i++) {
+        for (int j = 0; j < b_cols; j++){
+            assert(c[i * b_cols + j] == test_res[i * b_cols + j]);
+        }
+    }
+
+    cout << "Result matrix:" << endl;
+    for (int i = 0; i < a_rows; i++) {
+        for (int j = 0; j < b_cols; j++) {
+            cout << c[i * b_cols + j] << ", ";
+        }
+        cout << endl;
+    }
+}

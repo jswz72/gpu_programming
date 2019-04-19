0e323a4bfee239b8fcb287708b4357b42a8e7105
jswz72 <jacobdasword@gmail.com>
2019-04-19 16:23:06 -0400

    restructure

--- vocab_graph/gpu/review_and_recommend/ints/test.cu
+++ vocab_graph/gpu/review_and_recommend/ints/test.cu
@@ -14,85 +14,78 @@ using std::endl;
 #define NUM_ASYNCHRONOUS_ITERATIONS 20  // Number of async loop iterations before attempting to read results back
 
 // Check whether all verticies done
-bool allFinalizedVertices(bool *finalizedVertices, int numVertices) {
+bool all_vertices_done(bool *finished_verts, int num_vtx) {
 
-    for (int i = 0; i < numVertices; i++)
-        if (finalizedVertices[i] == true)
+    for (int i = 0; i < num_vtx; i++)
+        if (finished_verts[i] == true)
             return false;
 
     return true;
 }
 
-//Round a / b to ceiling
-int iDivUp(int a, int b)
-{
-    return (a % b != 0) ? (a / b + 1) : (a / b);
+// division ceiling
+int div_ceil(int numer, int denom) {
+    return (numer % denom != 0) ? (numer / denom + 1) : (numer / denom);
 }
 
-/*************************/
-/* ARRAY INITIALIZATIONS */
-/*************************/
-__global__ void initializeArrays(bool * __restrict__ d_finalizedVertices, 
-        int* __restrict__ d_shortestDistances, 
-        int* __restrict__ d_updatingShortestDistances, const int sourceVertex, 
-        const int numVertices) {
+__global__ void init_sssp_arrs(bool * __restrict__ d_finished_verts, 
+        int* __restrict__ d_dists, 
+        int* __restrict__ d_update_dists, const int source, 
+        const int num_vtx) {
 
     int tid = blockIdx.x * blockDim.x + threadIdx.x;
-    if (tid < numVertices) {
-        if (sourceVertex == tid) {
-            d_finalizedVertices[tid] = true;
-            d_shortestDistances[tid] = 0;
-            d_updatingShortestDistances[tid] = 0;
+    if (tid < num_vtx) {
+        if (source == tid) {
+            d_finished_verts[tid] = true;
+            d_dists[tid] = 0;
+            d_update_dists[tid] = 0;
         }
         else {
-            d_finalizedVertices[tid] = false;
-            d_shortestDistances[tid] = INT_MAX;
-            d_updatingShortestDistances[tid] = INT_MAX;
+            d_finished_verts[tid] = false;
+            d_dists[tid] = INT_MAX;
+            d_update_dists[tid] = INT_MAX;
         }
     }
 }
 
 // First portion GPU work for djikstra's
-__global__  void Kernel1(const int * __restrict__ vertexArray, 
-        const int* __restrict__ edgeArray, const int * __restrict__ weightArray,
-        bool * __restrict__ finalizedVertices, int* __restrict__ shortestDistances,
-        int * __restrict__ updatingShortestDistances, const int numVertices, 
-        const int numEdges) {
+__global__  void Kernel1(const int * __restrict__ beg_pos, 
+        const int* __restrict__ adj_list, const int * __restrict__ weights,
+        bool * __restrict__ finished_verts, int* __restrict__ dists,
+        int * __restrict__ update_dists, const int num_vtx) {
 
     int tid = blockIdx.x*blockDim.x + threadIdx.x;
-    if (tid < numVertices) {
-        if (finalizedVertices[tid] == true) {
-            finalizedVertices[tid] = false;
-            int edgeStart = vertexArray[tid];
-            int edgeEnd = vertexArray[tid + 1];
-            for (int edge = edgeStart; edge < edgeEnd; edge++) {
-                int nid = edgeArray[edge];
-                atomicMin(&updatingShortestDistances[nid], 
-                        shortestDistances[tid] + weightArray[edge]);
+    if (tid < num_vtx) {
+        if (finished_verts[tid] == true) {
+            finished_verts[tid] = false;
+            for (int edge = beg_pos[tid]; edge < beg_pos[tid + 1]; edge++) {
+                int other = adj_list[edge];
+                atomicMin(&update_dists[other], 
+                        dists[tid] + weights[edge]);
             }
         }
     }
 }
 
 // Second portion of GPU work for djikstra's
-__global__  void Kernel2(const int * __restrict__ vertexArray, 
-        const int * __restrict__ edgeArray, 
-        const int* __restrict__ weightArray, bool * __restrict__ finalizedVertices,
-        int* __restrict__ shortestDistances, 
-        int* __restrict__ updatingShortestDistances, const int numVertices) {
+__global__  void Kernel2(const int * __restrict__ beg_pos, 
+        const int * __restrict__ adj_list, 
+        const int* __restrict__ weights, bool * __restrict__ finished_verts,
+        int* __restrict__ dists, 
+        int* __restrict__ update_dists, const int num_vtx) {
 
     int tid = blockIdx.x * blockDim.x + threadIdx.x;
-    if (tid < numVertices) {
-        if (shortestDistances[tid] > updatingShortestDistances[tid]) {
-            shortestDistances[tid] = updatingShortestDistances[tid];
-            finalizedVertices[tid] = true; }
-        updatingShortestDistances[tid] = shortestDistances[tid];
+    if (tid < num_vtx) {
+        if (dists[tid] > update_dists[tid]) {
+            dists[tid] = update_dists[tid];
+            finished_verts[tid] = true; }
+        update_dists[tid] = dists[tid];
     }
 }
 
 // Main function handling djikstra's kernels
-void dijkstraGPU(int *beg_pos, int *adj_list, int *weights, const int sourceVertex, int * __restrict__ h_shortestDistances, int num_vtx, int num_edge) {
-
+void dijkstraGPU(int *beg_pos, int *adj_list, int *weights, const int source, 
+        int * __restrict__ dists, int num_vtx, int num_edge) {
     double s1 = wtime();
     int *d_beg_pos;
     HANDLE_ERR(cudaMalloc(&d_beg_pos, sizeof(int) * num_vtx));
@@ -109,128 +102,120 @@ void dijkstraGPU(int *beg_pos, int *adj_list, int *weights, const int sourceVert
                 cudaMemcpyHostToDevice));
 
     // Mask array
-    bool *d_finalizedVertices;
-    HANDLE_ERR(cudaMalloc(&d_finalizedVertices, sizeof(bool) * num_vtx));
+    bool *d_finished_verts;
+    HANDLE_ERR(cudaMalloc(&d_finished_verts, sizeof(bool) * num_vtx));
     // Cost array
-    int *d_shortestDistances;
-    HANDLE_ERR(cudaMalloc(&d_shortestDistances, sizeof(int) * num_vtx));
+    int *d_dists;
+    HANDLE_ERR(cudaMalloc(&d_dists, sizeof(int) * num_vtx));
     // Updating cost array
-    int *d_updatingShortestDistances;
-    HANDLE_ERR(cudaMalloc(&d_updatingShortestDistances, sizeof(int) * num_vtx));
+    int *d_update_dists;
+    HANDLE_ERR(cudaMalloc(&d_update_dists, sizeof(int) * num_vtx));
 
-    bool *h_finalizedVertices = (bool *)malloc(sizeof(bool) * num_vtx);
+    bool *finished_vtxs = (bool *)malloc(sizeof(bool) * num_vtx);
 
     double e1 = wtime();
     cout << "Time to init n copy: " << e1 - s1 << endl;
     double start = wtime();
     // Mask to 0's, cost and updating arrays to inf
-    initializeArrays 
-        <<<iDivUp(num_vtx, 16), 16 >>>
-        (d_finalizedVertices, d_shortestDistances, d_updatingShortestDistances, sourceVertex, num_vtx);
-    HANDLE_ERR(cudaPeekAtLastError());
+    init_sssp_arrs <<<div_ceil(num_vtx, 16), 16 >>>
+        (d_finished_verts, d_dists, d_update_dists, source, num_vtx);
     HANDLE_ERR(cudaDeviceSynchronize());
 
-    HANDLE_ERR(cudaMemcpy(h_finalizedVertices, d_finalizedVertices, 
+    HANDLE_ERR(cudaMemcpy(finished_vtxs, d_finished_verts, 
                 sizeof(bool) * num_vtx, cudaMemcpyDeviceToHost));
 
-    while (!allFinalizedVertices(h_finalizedVertices, num_vtx)) {
+    while (!all_vertices_done(finished_vtxs, num_vtx)) {
         // --- In order to improve performance, we run some number of iterations without reading the results.  This might result
         //     in running more iterations than necessary at times, but it will in most cases be faster because we are doing less
         //     stalling of the GPU waiting for results.
         for (int asyncIter = 0; asyncIter < NUM_ASYNCHRONOUS_ITERATIONS; 
                 asyncIter++) {
-            Kernel1 <<<iDivUp(num_vtx, 16), 16 >>>(d_beg_pos, d_adj_list, 
-                    d_weights, d_finalizedVertices, d_shortestDistances, d_updatingShortestDistances, num_vtx, num_edge);
-            HANDLE_ERR(cudaPeekAtLastError());
+            Kernel1 <<<div_ceil(num_vtx, 16), 16 >>>(d_beg_pos, d_adj_list, 
+                    d_weights, d_finished_verts, d_dists, d_update_dists, num_vtx);
             HANDLE_ERR(cudaDeviceSynchronize());
-            Kernel2 <<<iDivUp(num_vtx, 16), 16 >>>(d_beg_pos, d_adj_list, 
-                d_weights, d_finalizedVertices, d_shortestDistances,
-                d_updatingShortestDistances, num_vtx);
-            HANDLE_ERR(cudaPeekAtLastError());
+            Kernel2 <<<div_ceil(num_vtx, 16), 16 >>>(d_beg_pos, d_adj_list, 
+                d_weights, d_finished_verts, d_dists,
+                d_update_dists, num_vtx);
             HANDLE_ERR(cudaDeviceSynchronize());
         }
-
-        HANDLE_ERR(cudaMemcpy(h_finalizedVertices, d_finalizedVertices,
+        HANDLE_ERR(cudaMemcpy(finished_vtxs, d_finished_verts,
                 sizeof(bool) * num_vtx, cudaMemcpyDeviceToHost));
     }
     double end = wtime();
     double s2 = wtime();
     cout << "Inner GPU time: " << end - start << endl;
 
-    HANDLE_ERR(cudaMemcpy(h_shortestDistances, d_shortestDistances, sizeof(int) * num_vtx, cudaMemcpyDeviceToHost));
+    HANDLE_ERR(cudaMemcpy(dists, d_dists, sizeof(int) * num_vtx, cudaMemcpyDeviceToHost));
 
-    free(h_finalizedVertices);
+    free(finished_vtxs);
 
     HANDLE_ERR(cudaFree(d_beg_pos));
     HANDLE_ERR(cudaFree(d_adj_list));
     HANDLE_ERR(cudaFree(d_weights));
-    HANDLE_ERR(cudaFree(d_finalizedVertices));
-    HANDLE_ERR(cudaFree(d_shortestDistances));
-    HANDLE_ERR(cudaFree(d_updatingShortestDistances));
+    HANDLE_ERR(cudaFree(d_finished_verts));
+    HANDLE_ERR(cudaFree(d_dists));
+    HANDLE_ERR(cudaFree(d_update_dists));
     double e2 = wtime();
     cout << "Copy out and free time: " << e2 - s2 << endl;
 }
 
 // Get index of vertex not included in path with min dist
-int minDistance(int *shortestDistances, bool *finalizedVertices, 
-        const int sourceVertex, const int N) {
-    int minIndex = sourceVertex;
+int min_dist(int *dists, bool *finished_verts, 
+        const int source, const int N) {
+    int minIndex = source;
     int min = INT_MAX;
     for (int v = 0; v < N; v++)
-        if (finalizedVertices[v] == false && shortestDistances[v] <= min) {
-            min = shortestDistances[v];
+        if (finished_verts[v] == false && dists[v] <= min) {
+            min = dists[v];
             minIndex = v;
         }
     return minIndex;
 }
 
-void dijkstraCPU(int *beg_pos, int *adj_list, int *weights, int *h_shortestDistances, int sourceVertex, const int N) {
+void dijkstraCPU(int *beg_pos, int *adj_list, int *weights, int *dists, int source, const int N) {
 
-    // --- h_finalizedVertices[i] is true if vertex i is included in the shortest path tree
+    // --- finished_vtxs[i] is true if vertex i is included in the shortest path tree
     //     or the shortest distance from the source node to i is finalized
-    bool *h_finalizedVertices = (bool *)malloc(N * sizeof(bool));
+    bool *finished_vtxs = (bool *)malloc(N * sizeof(bool));
 
     for (int i = 0; i < N; i++) {
-        h_shortestDistances[i] = INT_MAX;
-        h_finalizedVertices[i] = false;
+        dists[i] = INT_MAX;
+        finished_vtxs[i] = false;
     }
 
-    h_shortestDistances[sourceVertex] = 0;
+    dists[source] = 0;
 
-    for (int iterCount = 0; iterCount < N - 1; iterCount++) {
-
-        int currentVertex = minDistance(h_shortestDistances, h_finalizedVertices,
-                sourceVertex, N);
-
-        h_finalizedVertices[currentVertex] = true;
-
-        // --- Relaxation loop
+    for (int i = 0; i < N - 1; i++) {
+        int cur = min_dist(dists, finished_vtxs, source, N);
+        finished_vtxs[cur] = true;
+        // Relaxation?
         for (int v = 0; v < N; v++) {
-
-            // --- Update dist[v] only if it is not in h_finalizedVertices, there is an edge
-            //     from u to v, and the cost of the path from the source vertex to v through
-            //     currentVertex is smaller than the current value of h_shortestDistances[v]
+            // Update if not finished, edge exits, and cost lower than cur cost
             bool found = false;
             int idx = 0;
-            for (int i = beg_pos[currentVertex]; i < beg_pos[currentVertex + 1]; 
+            for (int i = beg_pos[cur]; i < beg_pos[cur + 1]; 
                     i++) {
                 if (v == adj_list[i]) {
                     found = true;
                     idx = i;
                 }
             }
-            bool update = !h_finalizedVertices[v] && found &&
-                h_shortestDistances[currentVertex] != INT_MAX &&
-                h_shortestDistances[currentVertex] + weights[idx] <
-                    h_shortestDistances[v];
+            bool update = !finished_vtxs[v] && found &&
+                dists[cur] != INT_MAX &&
+                dists[cur] + weights[idx] <
+                    dists[v];
             if (update)
-                h_shortestDistances[v] = h_shortestDistances[currentVertex]
+                dists[v] = dists[cur]
                     + weights[idx];
         }
     }
 }
 
 int main(int argc, char **argv) {
+    if (argc < 2) { 
+        cout << "Enter source idx" << endl;
+        return -1;
+    }
     int source_vertex = atoi(argv[1]);
     graph<long, long, double, long, long, double> *csr = 
         new graph<long, long, double, long, long, double>
@@ -249,16 +234,18 @@ int main(int argc, char **argv) {
     for (int i = 0; i < csr->edge_count; i++) adj_list[i] = (int) csr->csr[i];
     for (int i = 0; i < csr->edge_count; i++) weight[i] = (int) (csr->weight[i] * 1000);
 
-    double cpu_start = wtime();
-    dijkstraCPU(beg_pos, adj_list, weight, shortest_dist_cpu, source_vertex, csr->vert_count);
-    double cpu_end = wtime();
-    cout << "CPU completed in " << cpu_end - cpu_start << endl;
-    
+        
     int *shortest_dist_gpu = (int *) malloc(csr->vert_count * sizeof(int));
     double gpu_start = wtime();
     dijkstraGPU(beg_pos, adj_list, weight, source_vertex, shortest_dist_gpu, csr->vert_count, csr->edge_count);
     double gpu_end = wtime();
     cout << "GPU completed in " << gpu_end - gpu_start << endl;
+
+    double cpu_start = wtime();
+    dijkstraCPU(beg_pos, adj_list, weight, shortest_dist_cpu, source_vertex, csr->vert_count);
+    double cpu_end = wtime();
+    cout << "CPU completed in " << cpu_end - cpu_start << endl;
+
 
     for (int i = 0; i < csr->vert_count; i++) {
         if (shortest_dist_gpu[i] != shortest_dist_cpu[i]) {
